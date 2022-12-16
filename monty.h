cb0ca7af533e8a9da7e47efa5ff9f149023fc97d
https://github.com/ogundeyidaniel <ogundeyidaniel@outlook.com>
2022-12-15 20:16:34 -0800

    montyy

--- monty.h
+++ monty.h
@@ -77,4 +77,41 @@ void sub_top_two(stack_t **stack, unsigned int line_number);
 void div_top_two(stack_t **stack, unsigned int line_number);
 void mul_top_two(stack_t **stack, unsigned int line_number);
 void mod_top_two(stack_t **stack, unsigned int line_number);
+extern global_var var_global;
+
+void read_file(char *filename, stack_t **stack);
+char *parse_line(char *line, stack_t **stack, unsigned int line_number);
+typedef void (*instruct_func)(stack_t **stack, unsigned int line_number);
+instruct_func get_op_func(char *str);
+ssize_t getline(char **lineptr, size_t *n, FILE *stream);
+void addqueue(stack_t **head, int n);
+void f_queue(stack_t **head, unsigned int counter);
+
+/*Fuxntions Monty*/
+void _pall(stack_t **stack, unsigned int line_number);
+void _push(stack_t **stack, unsigned int line_number);
+void _pint(stack_t **stack, unsigned int line_number);
+void _pop(stack_t **stack, unsigned int line_number);
+void _swap(stack_t **stack, unsigned int line_number);
+void _nop(stack_t **stack, unsigned int line_number);
+void _rotl(stack_t **stack, unsigned int line_number);
+void _rotr(stack_t **stack, unsigned int line_number);
+
+/*Functions string ascci */
+void _pchar(stack_t **stack, unsigned int line_number);
+void _pstr(stack_t **stack, unsigned int line_number);
+
+/*Functions math*/
+void _sub(stack_t **stack, unsigned int line_number);
+void _add(stack_t **stack, unsigned int line_number);
+void _mul(stack_t **stack, unsigned int line_number);
+void _div(stack_t **stack, unsigned int line_number);
+void _mod(stack_t **stack, unsigned int line_number);
+
+/*Utility functions*/
+void free_dlistint(stack_t *head);
+void dobfree(stack_t **stack);
+void dobfree2(stack_t **stack);
+int _isalpha(int c);
+
 #endif /* _MONTY_H */
